5212fc160e89a17599fc4ec5d96372976308db23
wendale1231 <spooderm5@gmail.com>
2018-10-23 08:42:58 +0800

    setting index as rediractor

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>REDIRECTING</title>
+    <script src="main.js"></script>
+</head>
+<body>
+    <center>
+        <h1>REDIRECTING...</h1>
+    </center>
+</body>
+</html>
