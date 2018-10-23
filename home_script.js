daa1c8a9270dc8f5485ab77a6cbca61b7d462435
wendale1231 <spooderm5@gmail.com>
2018-10-23 16:10:26 +0800

    another modification

--- home_script.js
+++ home_script.js
@@ -1,5 +1,5 @@
 function myFunction(sticky,navbar) {
-    if (window.pageYOffset >= sticky) {
+    if (window.pageYOffset >= 200) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
